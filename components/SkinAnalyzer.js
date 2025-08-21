2ea47422112bbc7dcccd644dac77ef2226aae08a
codingmaster094 <codingmaster094@gmail.com>
2025-08-21 12:48:24 +0530

    add qr code

--- components/SkinAnalyzer.js
+++ components/SkinAnalyzer.js
@@ -135,13 +135,21 @@ export default function SkinAnalyzer() {
           {mode === "upload" ? (
             <>
               <label className="block text-sm font-medium mb-2">Face image *</label>
-              <input
-                ref={imgInputRef}
-                type="file"
-                accept="image/*"
-                onChange={(e) => setImageFile(e.target.files?.[0] || null)}
-                className="block w-full"
-              />
+                {
+                !imgPreviewUrl && 
+                <div className="flex items-center justify-center w-full">
+                    <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                            <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                            </svg>
+                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                        </div>
+                        <input  ref={imgInputRef} accept="image/*"  onChange={(e) => setImageFile(e.target.files?.[0] || null)} id="dropzone-file" type="file" className="hidden" />
+                    </label>
+                </div> 
+              }
             </>
           ) : (
             <>
@@ -298,6 +306,7 @@ export default function SkinAnalyzer() {
           {/* Recommended Products (using concerns as rows as you had) */}
           <div>
             <h3 className="text-lg font-semibold mb-2">Recommended Products</h3>
+            {console.log("data" , data)}
             {data.concerns ? (
               <ProductsTable
                 rows={Object.entries(data.concerns).map(([name, values]) => ({
@@ -320,6 +329,8 @@ export default function SkinAnalyzer() {
 }
 
 function ProductsTable({ rows }) {
+  console.log("rows" , rows);
+  
   const cols = useMemo(() => {
     const set = new Set();
     rows.forEach((r) => Object.keys(r || {}).forEach((k) => set.add(k)));
